bf4e78d50a67511e995e0e3302e75ff7709d5153
AbdRaqeeb <Ajaorqb@gmail.com>
2020-04-20 19:25:06 +0100

    task 3

--- GroceryApps/app/src/main/java/com/example/groceryapps/Activity2.java
+++ GroceryApps/app/src/main/java/com/example/groceryapps/Activity2.java
@@ -4,25 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Your body here";
+                String shareSub = "Your Subject here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share using"));
+            }
+        });
+
         Intent intent = getIntent();
         String text =  intent.getStringExtra(MainActivity.EXTRA_TEXT);
         int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+
+        String text2 =  intent.getStringExtra(MainActivity.EXTRA_TEXT2);
+        int number2 = intent.getIntExtra(MainActivity.EXTRA_NUMBER2, 0);
+
+        String text3 =  intent.getStringExtra(MainActivity.EXTRA_TEXT3);
+        int number3 = intent.getIntExtra(MainActivity.EXTRA_NUMBER3, 0);
+
+        int sum = intent.getIntExtra(MainActivity.EXTRA_NUMBER4, 0);
 
 
         TextView textView2 = (TextView) findViewById(R.id.textView2);
         TextView textView3 = (TextView) findViewById(R.id.textView3);
 
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+
+        TextView textView6 = (TextView) findViewById(R.id.textView6);
+        TextView textView7 = (TextView) findViewById(R.id.textView7);
+
+        TextView textView8 = (TextView)  findViewById(R.id.textView8);
+
         textView2.setText(text);
         textView3.setText("" + number);
+        textView4.setText(text2);
+        textView5.setText("" + number2);
+        textView6.setText(text3);
+        textView7.setText("" + number3);
+        textView8.setText("" + sum);
 
     }
 }
--- GroceryApps/app/src/main/java/com/example/groceryapps/MainActivity.java
+++ GroceryApps/app/src/main/java/com/example/groceryapps/MainActivity.java
@@ -11,6 +11,11 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_TEXT = "com.example.groceryapps.EXTRA_TEXT";
     public static final String EXTRA_NUMBER = "com.example.groceryapps.EXTRA_NUMBER";
+    public static final String EXTRA_TEXT2 = "com.example.groceryapps.EXTRA_TEXT2";
+    public static final String EXTRA_TEXT3 = "com.example.groceryapps.EXTRA_TEXT3";;
+    public static final String EXTRA_NUMBER2 = "com.example.groceryapps.EXTRA_NUMBER2";
+    public static final String EXTRA_NUMBER3 = "com.example.groceryapps.EXTRA_NUMBER3";
+    public static final String EXTRA_NUMBER4 = "com.example.groceryapps.EXTRA_NUMBER4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +33,31 @@ public class MainActivity extends AppCompatActivity {
         EditText editText1 = (EditText) findViewById(R.id.edit_text1);
         String text = editText1.getText().toString();
 
+        EditText editText3 = (EditText) findViewById(R.id.edit_text3);
+        String text2 = editText3.getText().toString();
+
+        EditText editText5 = (EditText) findViewById(R.id.edit_text5);
+        String text3 = editText5.getText().toString();
+
         EditText editText2 = (EditText) findViewById(R.id.edit_text2);
         int number = Integer.parseInt(editText2.getText().toString());
 
+        EditText editText4 = (EditText) findViewById(R.id.edit_text4);
+        int number2 = Integer.parseInt(editText4.getText().toString());
+
+        EditText editText6 = (EditText) findViewById(R.id.edit_text6);
+        int number3 = Integer.parseInt(editText6.getText().toString());
+
+        int sum = number+number2 +number3;
+
         Intent intent = new Intent(this, Activity2.class);
         intent.putExtra(EXTRA_TEXT, text);
+        intent.putExtra(EXTRA_TEXT2, text2);
+        intent.putExtra(EXTRA_TEXT3, text3);
         intent.putExtra(EXTRA_NUMBER, number);
-
+        intent.putExtra(EXTRA_NUMBER2, number2);
+        intent.putExtra(EXTRA_NUMBER3, number3);
+        intent.putExtra(EXTRA_NUMBER4, sum);
         startActivity(intent);
     }
 }
